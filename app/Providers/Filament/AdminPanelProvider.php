c9e942104553b17f16d60614ee55a0bea72c2f59
trungdung8311-art <trungdung8311@gmail.com>
2026-04-05 10:01:28 +0700

    install filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => '#87ceeb', // change primary color
+                'primary' => '#87ceeb', // change primary colour
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
